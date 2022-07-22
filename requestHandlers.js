1d4048226ed9fc0aeaf990c4785e9a871bf3b287
[ferdinandmulimi] <[ferdinand.mulimi@student.moringaschool.com]>
2022-07-22 12:15:08 +0300

    handler, router and index.js files updating

---
+++ requestHandlers.js
@@ -0,0 +1,20 @@
+// function for handling the business logic for index.html
+function index (){
+   console.log("Request handler for index was called.")
+   // Function that will delay for 10 seconds before returning the text.
+   function sleep(milliseconds){
+       var startTime = new Date().getTime()
+       while (new Date().getTime() < startTime + milliseconds){}
+   };
+   sleep(10000);
+   return "Welcome to Delani Studio"
+}
+
+// function for handling the business logic for  portfolio.html
+function portfolio(){
+   console.log("Request for handler for portfolio was called.")
+   return "These are some of our portfolio projects"
+}
+
+exports.index = index;
+exports.portfolio = portfolio;
